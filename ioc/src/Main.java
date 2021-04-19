0b7fcd1341e1d4bd6ecf888de607b79faba25bcf
Thierry Nambinina <rthierrynambinina@gmail.com>
2021-04-19 18:34:12 +0300

    static ioc

--- ioc/src/Main.java
+++ ioc/src/Main.java
@@ -1,5 +1,15 @@
+import dao.DaoImpl;
+import metier.MetierImpl;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Static IOC");
+        /**
+         * weak coupling
+         */
+        MetierImpl metierImpl = new MetierImpl();
+        DaoImpl daoImpl = new DaoImpl();
+        metierImpl.setDao(daoImpl);
+        System.out.println(metierImpl.calcul());
     }
 }
---
+++ ioc/src/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class DaoImpl implements IDao {
+    @Override
+    public double getData() {
+        /**
+         * Connect to database
+         */
+        return 98;
+    }
+}
