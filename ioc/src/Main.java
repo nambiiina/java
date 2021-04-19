bc5506aa8879b0d2211aca7fbcee662f4ad2d70a
Thierry Nambinina <rthierrynambinina@gmail.com>
2021-04-19 20:05:49 +0300

    dynamic ioc

--- ioc/src/Main.java
+++ ioc/src/Main.java
@@ -1,16 +1,31 @@
-import dao.DaoImpl;
-import metier.MetierImpl;
+import dao.IDao;
+import metier.IMetier;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("IOC");
-        /**
-         * Static IOC
-         * Strong coupling
-         */
-        MetierImpl metierImpl = new MetierImpl();
-        DaoImpl daoImpl = new DaoImpl();
-        metierImpl.setDao(daoImpl);
-        System.out.println(metierImpl.calcul());
+        dynamicIoc();
+    }
+    private static void dynamicIoc() {
+        try {
+            Scanner scanner = new Scanner(new File("config.txt"));
+            String daoClassName = scanner.nextLine();
+            Class cDao = Class.forName(daoClassName);
+            IDao dao = (IDao) cDao.newInstance();
+
+            String metierClassName = scanner.nextLine();
+            Class cMetier = Class.forName(metierClassName);
+            IMetier metier = (IMetier) cMetier.newInstance();
+
+            Method method = cMetier.getMethod("setDao", IDao.class);
+            method.invoke(metier, dao);
+
+            System.out.println(metier.calcul());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
