a328ae3bc830dc2c29dc59d7fe4fd9fbf6b61d3b
Thierry Nambinina <rthierrynambinina@gmail.com>
2021-04-30 21:12:29 +0300

    add resources and profile configuration

--- my-maven-app/src/main/java/org/example/demo/App.java
+++ my-maven-app/src/main/java/org/example/demo/App.java
@@ -1,13 +1,26 @@
 package org.example.demo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        Properties vProp = new Properties();
+        InputStream vInputStream = null;
+        try {
+            vInputStream = App.class.getResourceAsStream("/application.properties");
+            vProp.load(vInputStream);
+        } finally {
+            if (vInputStream != null) vInputStream.close();
+        }
+        System.out.println("Application version : " + vProp.getProperty("org.example.demo.version", "?"));
     }
 }
