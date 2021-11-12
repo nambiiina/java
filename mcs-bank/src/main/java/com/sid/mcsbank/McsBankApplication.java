ccf03e558095393f81864c68931df814a57c9144
Thierry <rthierrynambinina@gmail.com>
2021-11-12 18:05:06 +0300

    mcs socle

---
+++ mcs-bank/src/main/java/com/sid/mcsbank/McsBankApplication.java
@@ -0,0 +1,13 @@
+package com.sid.mcsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsBankApplication.class, args);
+    }
+
+}
---
+++ mcs-bank/src/test/java/com/sid/mcsbank/McsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.mcsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
