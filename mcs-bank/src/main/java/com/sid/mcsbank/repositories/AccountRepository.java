e80df7113817fdbfe37fdc947f6bec955467da0d
Thierry <rthierrynambinina@gmail.com>
2021-11-28 15:24:33 +0300

    spring data rest (search)

--- mcs-bank/src/main/java/com/sid/mcsbank/repositories/AccountRepository.java
+++ mcs-bank/src/main/java/com/sid/mcsbank/repositories/AccountRepository.java
@@ -1,10 +1,19 @@
 package com.sid.mcsbank.repositories;
 
 import com.sid.mcsbank.entities.Account;
+import com.sid.mcsbank.entities.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 //http://localhost:8080/api/accounts?page=0&size=2&sort=balance,asc
 @RepositoryRestResource
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    //http://localhost:8080/api/accounts/search/byType?t=SAVINGS
+    @RestResource(path = "/byType")
+    //http://localhost:8080/api/accounts/search/findByAccountType?accountType=CURRENT
+    List<Account> findByAccountType (@Param("t") AccountType accountType);
 }
