3653fad000c38acc12657dc924f648d60dded00d
Thierry <rthierrynambinina@gmail.com>
2021-12-27 10:09:00 +0300

    add pagination

--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/web/PatientController.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/web/PatientController.java
@@ -3,9 +3,12 @@ package com.example.springmvcthymeleaf.web;
 import com.example.springmvcthymeleaf.dao.PatientRepository;
 import com.example.springmvcthymeleaf.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,9 +24,11 @@ public class PatientController {
     }
 
     @GetMapping(path = "/patients")
-    public String listPatients(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String listPatients(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size) {
+        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patients", patientPage.getContent());
+        model.addAttribute("pages", new int[patientPage.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 
