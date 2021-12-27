e7df0919d1a8520e3ece8af699485ead3cefde3c
Thierry <rthierrynambinina@gmail.com>
2021-12-27 11:21:19 +0300

    research a patient

--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/dao/PatientRepository.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/dao/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.example.springmvcthymeleaf.dao;
 
 import com.example.springmvcthymeleaf.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNameContains(String keyword, Pageable pageable);
 }
--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/web/PatientController.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/web/PatientController.java
@@ -10,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 public class PatientController {
 
@@ -24,11 +22,14 @@ public class PatientController {
     }
 
     @GetMapping(path = "/patients")
-    public String listPatients(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size) {
-        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+    public String listPatients(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size, @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        //Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+        Page<Patient> patientPage = patientRepository.findByNameContains(keyword, PageRequest.of(page, size));
         model.addAttribute("patients", patientPage.getContent());
         model.addAttribute("pages", new int[patientPage.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("size", size);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 
