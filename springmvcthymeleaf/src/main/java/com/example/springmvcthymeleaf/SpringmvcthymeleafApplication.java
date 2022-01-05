67134253c6835236541bac177447d05772db6151
Thierry <rthierrynambinina@gmail.com>
2022-01-05 17:49:12 +0300

    - integrate spring security - basic configuration of security - login & logout - use in memory authentication

--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/SpringmvcthymeleafApplication.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/SpringmvcthymeleafApplication.java
@@ -9,9 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringmvcthymeleafApplication {
+
+    private final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345674890";
+    private final Random rand = new Random();
+    private final Set<String> identifiers = new HashSet<String>();
 
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcthymeleafApplication.class, args);
@@ -20,11 +27,24 @@ public class SpringmvcthymeleafApplication {
     @Bean
     public CommandLineRunner run(PatientRepository patientRepository) throws Exception {
         return (String[] args) -> {
-            patientRepository.save(new Patient(null, "Thierry", new Date(), false, 6));
-            patientRepository.save(new Patient(null, "Nambinina", new Date(), false, 46));
-            patientRepository.save(new Patient(null, "Ninah", new Date(), false, 35));
-            patientRepository.save(new Patient(null, "Hasimino", new Date(), false, 14));
-            patientRepository.findAll().forEach(p -> System.out.println(p.getName()));
+            patientRepository.save(new Patient(null, randomIdentifier(), new Date(), false, new Random().nextInt(100-4) + 4));
+            patientRepository.save(new Patient(null, randomIdentifier(), new Date(), true, new Random().nextInt(100-4) + 4));
+            patientRepository.save(new Patient(null, randomIdentifier(), new Date(), false, new Random().nextInt(100-4) + 4));
+//            patientRepository.findAll().forEach(p -> System.out.println(p.getName()));
         };
+    }
+
+    public String randomIdentifier() {
+        StringBuilder builder = new StringBuilder();
+        while(builder.toString().length() == 0) {
+            int length = rand.nextInt(5)+5;
+            for(int i = 0; i < length; i++) {
+                builder.append(lexicon.charAt(rand.nextInt(lexicon.length())));
+            }
+            if(identifiers.contains(builder.toString())) {
+                builder = new StringBuilder();
+            }
+        }
+        return builder.toString();
     }
 }
