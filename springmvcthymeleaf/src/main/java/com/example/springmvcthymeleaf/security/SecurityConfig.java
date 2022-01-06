91b0f8cdbad4ebd8ee6ff97b35dc78ff2196f865
Thierry <rthierrynambinina@gmail.com>
2022-01-06 10:41:05 +0300

    - passEncoder = Bcrypt - ajust menu

--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/security/SecurityConfig.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/security/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.example.springmvcthymeleaf.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        super.configure(auth); // by default configure one "user" with generated password
-        auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("USER"); // => noop disable encoded password
-        auth.inMemoryAuthentication().withUser("user2").password("{noop}1234").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER", "ADMIN");
+        PasswordEncoder passwordEncoder = passwordEncoder();
+//        auth.inMemoryAuthentication().withUser("user0").password("{noop}1234").roles("USER"); // => noop disable encoded password
+        System.out.println("#####################################");
+        System.out.println(passwordEncoder.encode("1234"));
+        System.out.println("#####################################");
+        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
+        auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN");
     }
 
     @Override
@@ -25,5 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeHttpRequests()
             .anyRequest()
             .authenticated();
+        http.csrf(); // => enable by default
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
