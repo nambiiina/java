917a5826e3d086cae9fb0899d6091043d1f7a313
Thierry <rthierrynambinina@gmail.com>
2022-01-07 19:10:58 +0300

    jdbc authentication

--- springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/security/SecurityConfig.java
+++ springmvcthymeleaf/src/main/java/com/example/springmvcthymeleaf/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springmvcthymeleaf.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,8 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        super.configure(auth); // by default configure one "user" with generated password
@@ -18,9 +25,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         System.out.println("#####################################");
         System.out.println(passwordEncoder.encode("1234"));
         System.out.println("#####################################");
-        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
+        /**
+         * inMemoryAuthentication
+         */
+        /*auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN");*/
+
+        /**
+         * jdbcAuthentication
+         */
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+            .authoritiesByUsernameQuery("select username as principal, role from users_roles where username=?")
+            .passwordEncoder(passwordEncoder)
+            .rolePrefix("ROLE_"); // => add this prefix in database roles
+
     }
 
     @Override
@@ -28,9 +49,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        super.configure(http); // => Enable security
         http.formLogin(); // => use form authentication by spring security
 //        http.httpBasic(); // => spring security ask to browser the request needs authentication
-        http.authorizeHttpRequests().antMatchers("/save**/**", "/delete**/**", "/add**/**", "/edit**/**").hasRole("ADMIN");
+        http.authorizeHttpRequests().antMatchers("/admin/**", "/save**/**", "/delete**/**", "/add**/**", "/edit**/**").hasRole("ADMIN");
         http.authorizeHttpRequests().antMatchers("/patients**/**").hasRole("USER");
-//        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.authorizeHttpRequests().antMatchers("/user/**").permitAll();
+        http.authorizeHttpRequests().anyRequest().authenticated();
         http.csrf(); // => enable by default
         http.exceptionHandling().accessDeniedPage("/notAuthorized");
     }
