7b2b2ad70f52a2e578ebaeb460a3180990733855
Thierry <rthierrynambinina@gmail.com>
2022-01-09 17:27:42 +0300

    create rest controller with CRUD operation for entity Account

---
+++ springrest/src/main/java/com/example/springrest/web/AccountRestController.java
@@ -0,0 +1,44 @@
+package com.example.springrest.web;
+
+import com.example.springrest.entities.Account;
+import com.example.springrest.repositories.AccountRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AccountRestController {
+
+    private final AccountRepository accountRepository;
+
+
+    public AccountRestController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @GetMapping(path = "/accounts")
+    public List<Account> accountList() {
+        return accountRepository.findAll();
+    }
+
+    @GetMapping(path = "/accounts/{code}")
+    public Account getAccount(@PathVariable(name = "code") Long code) {
+        return accountRepository.findById(code).get();
+    }
+
+    @PostMapping(path = "/accounts")
+    public Account save(@RequestBody Account account) {
+        return accountRepository.save(account);
+    }
+
+    @PutMapping(path = "/accounts/{code}")
+    public Account update(@PathVariable(name = "code") Long code, @RequestBody Account account) {
+        account.setCode(code);
+        return accountRepository.save(account);
+    }
+
+    @DeleteMapping(path = "/accounts/{code}")
+    public void delete(@PathVariable(name = "code") Long code) {
+        accountRepository.deleteById(code);
+    }
+}
