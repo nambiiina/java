84206707c6cfe11d3202506041da3f4a6e64e175
Thierry <rthierrynambinina@gmail.com>
2022-01-16 22:22:54 +0300

    - create repository rest resource for Company - configure h2 database - disabled spring cloud config and spring cloud discovery

--- spring-cloud/bourse/service-company/src/main/java/com/example/servicecompany/ServiceCompanyApplication.java
+++ spring-cloud/bourse/service-company/src/main/java/com/example/servicecompany/ServiceCompanyApplication.java
@@ -1,13 +1,29 @@
 package com.example.servicecompany;
 
+import com.example.servicecompany.dao.CompanyRepository;
+import com.example.servicecompany.entities.Company;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ServiceCompanyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceCompanyApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CompanyRepository companyRepository) {
+        return args -> {
+            Stream.of("Orange", "Ravinala", "Habibo").forEach(company -> {
+                companyRepository.save(new Company(null, company, 100+Math.random()*900));
+            });
+            companyRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
---
+++ spring-cloud/bourse/service-company/src/main/java/com/example/servicecompany/dao/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.example.servicecompany.dao;
+
+import com.example.servicecompany.entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
