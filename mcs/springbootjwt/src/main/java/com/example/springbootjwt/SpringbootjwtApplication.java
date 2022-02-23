1b8fe74184818577e81c91b0bdd97232f6315a22
Thierry <rthierrynambinina@gmail.com>
2022-02-23 22:48:07 +0300

    - create jpa entity - create repository for Student - configure app to connect to mysql - expose all jpa interface in rest api

--- mcs/springbootjwt/src/main/java/com/example/springbootjwt/SpringbootjwtApplication.java
+++ mcs/springbootjwt/src/main/java/com/example/springbootjwt/SpringbootjwtApplication.java
@@ -1,13 +1,34 @@
 package com.example.springbootjwt;
 
+import com.example.springbootjwt.entities.Student;
+import com.example.springbootjwt.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringbootjwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjwtApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(StudentRepository studentRepository) {
+        return args -> {
+          studentRepository.save(new Student(null, "Randria", "Thierry", LocalDate.now()));
+          studentRepository.save(new Student(null, "Randria", "Nambinina", LocalDate.now()));
+          studentRepository.save(new Student(null, "Razafi", "Ninah", LocalDate.now()));
+          studentRepository.save(new Student(null, "Razafi", "Hasimino", LocalDate.now()));
+
+          studentRepository.findAll().forEach(student -> {
+              System.out.println(student);
+          });
+        };
     }
 
 }
---
+++ mcs/springbootjwt/src/main/java/com/example/springbootjwt/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootjwt.repository;
+
+import com.example.springbootjwt.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
