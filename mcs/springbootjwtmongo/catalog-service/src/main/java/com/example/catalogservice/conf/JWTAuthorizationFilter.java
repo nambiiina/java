f1fe98b51e8904c18948fc2f27f610b0ea5c7205
Thierry <rthierrynambinina@gmail.com>
2022-03-27 16:25:13 +0300

    - Add resource authorization filter - Change port

--- mcs/springbootjwtmongo/catalog-service/src/main/java/com/example/catalogservice/conf/JWTAuthorizationFilter.java
+++ mcs/springbootjwtmongo/catalog-service/src/main/java/com/example/catalogservice/conf/JWTAuthorizationFilter.java
@@ -1,5 +1,13 @@
 package com.example.catalogservice.conf;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,14 +15,52 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("***********************");
-
-        String jwt = request.getHeader("Authorization");
-        if (jwt == null) throw new RuntimeException("Not Authorized !");
-        filterChain.doFilter(request, response);
+        // CORS
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-Width, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        else {
+            // Get Authorization
+            String jwt = request.getHeader(SecurityParams.JWT_HEADER_NAME);
+            // Verify if authorization is presents in header request
+            if (jwt == null || !jwt.startsWith(SecurityParams.TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            //Get token
+            String token = jwt.substring(SecurityParams.TOKEN_PREFIX.length());
+//            System.out.println(token);
+            //Create token verifier
+            Algorithm algorithm = Algorithm.HMAC256(SecurityParams.SECRET);
+            JWTVerifier verifier = JWT.require(algorithm)
+//                .withIssuer("oauth")
+                    .build();
+            //Verify token
+            DecodedJWT verify = verifier.verify(token);
+            //Decode token
+            DecodedJWT decodedJWT = JWT.decode(token);
+            String username = decodedJWT.getSubject();
+//            System.out.println(username);
+            List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
+//            System.out.println(roles);
+            Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+            for (String role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role));
+            }
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            //Authenticate the user
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            filterChain.doFilter(request, response);
+        }
     }
 }
---
+++ mcs/springbootjwtmongo/catalog-service/src/main/java/com/example/catalogservice/conf/SecurityParams.java
@@ -0,0 +1,8 @@
+package com.example.catalogservice.conf;
+
+public final class SecurityParams {
+    static final String JWT_HEADER_NAME = "Authorization";
+    static final String SECRET = "rthierrynambinina@gmail.com";
+    static final Long EXPIRATION = 864000L;
+    static final String TOKEN_PREFIX = "Bearer ";
+}
