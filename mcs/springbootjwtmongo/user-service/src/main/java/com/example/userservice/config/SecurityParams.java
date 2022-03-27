c1a5c1c2b7d7d20717f1c8be57ce12bf18109c9f
Thierry <rthierrynambinina@gmail.com>
2022-03-27 20:38:03 +0300

    Update config

--- mcs/springbootjwtmongo/user-service/src/main/java/com/example/userservice/config/SecurityParams.java
+++ mcs/springbootjwtmongo/user-service/src/main/java/com/example/userservice/config/SecurityParams.java
@@ -3,6 +3,6 @@ package com.example.userservice.config;
 public final class SecurityParams {
     static final String JWT_HEADER_NAME = "Authorization";
     static final String SECRET = "rthierrynambinina@gmail.com";
-    static final Long EXPIRATION = 864000L;
+    static final Long EXPIRATION = 10L*24*3600*1000;
     static final String TOKEN_PREFIX = "Bearer ";
 }
