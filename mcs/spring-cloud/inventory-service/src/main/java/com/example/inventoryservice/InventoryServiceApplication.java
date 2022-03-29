fae64ce4c2681647f62c9c0a874352965fec1eda
Thierry <rthierrynambinina@gmail.com>
2022-03-29 18:35:24 +0300

    create api for product

--- mcs/spring-cloud/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ mcs/spring-cloud/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,27 @@
 package com.example.inventoryservice;
 
+import com.example.inventoryservice.entities.Product;
+import com.example.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(new Product(null, "Alienware x14", 2347.99));
+            productRepository.save(new Product(null, "MacBook Air", 1129.00));
+            productRepository.save(new Product(null, "Legion 5i 17 - Phantom Blue & Shadow Black", 1999.00));
+            productRepository.save(new Product(null, "Surface Laptop 4", 799.99));
+        };
     }
 
 }
