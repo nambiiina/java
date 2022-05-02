d42a918866208d0eccfe47a43e0959d52cab01ec
Thierry <rthierrynambinina@gmail.com>
2022-05-02 17:36:42 +0300

    expose id for Product entity

--- mcs/spring-cloud/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ mcs/spring-cloud/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -15,8 +16,9 @@ public class InventoryServiceApplication {
     }
 
     @Bean
-    CommandLineRunner start(ProductRepository productRepository) {
+    CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration) {
         return args -> {
+            restConfiguration.exposeIdsFor(Product.class);
             productRepository.save(new Product(null, "Alienware x14", 2347.99));
             productRepository.save(new Product(null, "MacBook Air", 1129.00));
             productRepository.save(new Product(null, "Legion 5i 17 - Phantom Blue & Shadow Black", 1999.00));
