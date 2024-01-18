9740c871abb2b191d96979d1e1dd4aed667bc5c7
nambiiina <rthierrynambinina@gmail.com>
2024-01-18 10:44:38 +0300

    Delete orm-jpa-hibernate-spring-data/src directory

--- orm-jpa-hibernate-spring-data/src/main/java/com/example/demo/dao/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
