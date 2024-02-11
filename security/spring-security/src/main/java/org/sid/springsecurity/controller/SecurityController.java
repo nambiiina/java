997a2d36742d4eeda44eec51069565d742f25b3c
nambiiina <rthierrynambinina@gmail.com>
2024-02-11 09:25:34 +0300

    Use authorities instead roles

--- security/spring-security/src/main/java/org/sid/springsecurity/controller/SecurityController.java
+++ security/spring-security/src/main/java/org/sid/springsecurity/controller/SecurityController.java
@@ -1,14 +1,22 @@
 package org.sid.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/notAuthorized")
 public class SecurityController {
 
-    @GetMapping
+    @GetMapping("/authenticatedUser")
+//    @PreAuthorize("hasAuthority('ADMIN')")
+    public Authentication authentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("/notAuthorized")
     public String notAuthorized() {
         return "Not authorized";
     }
--- security/spring-security/src/main/java/org/sid/springsecurity/service/UserDetailsServiceImpl.java
+++ security/spring-security/src/main/java/org/sid/springsecurity/service/UserDetailsServiceImpl.java
@@ -3,13 +3,16 @@ package org.sid.springsecurity.service;
 import lombok.AllArgsConstructor;
 import org.sid.springsecurity.entity.AppRole;
 import org.sid.springsecurity.entity.AppUser;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,9 +24,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<AppUser> appUser = Optional.ofNullable(accountService.loadUserByUsername(username));
         if (appUser.isEmpty()) throw new UsernameNotFoundException(String.format("User %s not found", username));
+        /*return User
+                .withUsername(appUser.get().getUsername())
+                .password(appUser.get().getPassword())
+                .roles(appUser.get().getAppRoles().stream().map(AppRole::getRoleName).toArray(String[]::new)).build();*/
+
+        // Use authorities instead roles ROLE_USER => USER and hasAuthority to check
+        List<SimpleGrantedAuthority> authorities = appUser.get().getAppRoles().stream().map(appRole -> new SimpleGrantedAuthority(appRole.getRoleName())).toList();
         return User
                 .withUsername(appUser.get().getUsername())
                 .password(appUser.get().getPassword())
-                .roles(appUser.get().getAppRoles().stream().map(AppRole::getRoleName).toArray(String[]::new)).build();
+                .authorities(authorities).build();
     }
 }
