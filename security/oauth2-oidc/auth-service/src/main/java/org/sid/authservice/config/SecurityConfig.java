9957719d9526c4d1ff3484b15c95c573da615520
nambiiina <rthierrynambinina@gmail.com>
2024-02-15 20:30:37 +0300

    http basic auth

---
+++ security/oauth2-oidc/auth-service/src/main/java/org/sid/authservice/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.sid.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("user2").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("admin").password("{noop}admin").authorities("USER", "ADMIN")
+                .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .anyRequest().authenticated())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
---
+++ security/oauth2-oidc/auth-service/src/main/java/org/sid/authservice/controller/TestRestAPI.java
@@ -0,0 +1,16 @@
+package org.sid.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestRestAPI {
+
+    @GetMapping("/dataTest")
+    public Map<String, Object> dataTest() {
+        return Map.of("message", "Data test");
+    }
+
+}
