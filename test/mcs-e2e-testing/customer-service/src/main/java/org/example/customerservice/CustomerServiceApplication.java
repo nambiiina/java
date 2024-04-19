99b2964f68bf7e0ac00470a4693c484642dda6e5
nambiiina <rthierrynambinina@gmail.com>
2024-04-19 18:26:04 +0300

    Write repository test

--- test/mcs-e2e-testing/customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ test/mcs-e2e-testing/customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,42 @@
 package org.example.customerservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.customerservice.entities.Customer;
+import org.example.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Slf4j
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("!test")
+	CommandLineRunner setup(CustomerRepository customerRepository) {
+		return args -> {
+			customerRepository.save(Customer.builder()
+							.firstName("Randrianandrianina")
+							.lastName("Thierry")
+							.email("rthierry@mail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+							.firstName("Randrianandrianina")
+							.lastName("Nambinina")
+							.email("rnambinina@mail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+							.firstName("Randrianandrianina")
+							.lastName("Harti")
+							.email("rharti@mail.com")
+					.build());
+		};
 	}
 
 }
---
+++ test/mcs-e2e-testing/customer-service/src/test/java/org/example/customerservice/repository/CustomerRepositoryTest.java
@@ -0,0 +1,86 @@
+package org.example.customerservice.repository;
+
+import org.example.customerservice.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ActiveProfiles("test")
+@DataJpaTest
+/**
+ * 1- Configuration d'une base de données en mémoire
+ * 2- Configuration d'un contexte d'application restreint : Seuls les composants pertinents pour les tests du repo sont chargés dans le contexte.
+ * 3- Trasaction automatique qui effectue un rollback à chaque fin de test.
+ */
+class CustomerRepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    public void setUp() {
+        customerRepository.save(Customer.builder()
+                .firstName("Mohamed")
+                .lastName("Thierry")
+                .email("rthierry@mail.com")
+                .build());
+        customerRepository.save(Customer.builder()
+                .firstName("Imane")
+                .lastName("Nambinina")
+                .email("rnambinina@mail.com")
+                .build());
+        customerRepository.save(Customer.builder()
+                .firstName("Randrianandrianina")
+                .lastName("Harti")
+                .email("rharti@mail.com")
+                .build());
+    }
+
+    @Test
+    public void shouldFindCustomerByEmail() {
+        // GIVEN
+        String givenEmail = "rthierry@mail.com";
+
+        // WHEN
+        Optional<Customer> result = customerRepository.findByEmail(givenEmail);
+
+        // THEN
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    public void shouldNotFindCustomerByEmail() {
+        // GIVEN
+        String givenEmail = "xxx@mail.com";
+
+        // WHEN
+        Optional<Customer> result = customerRepository.findByEmail(givenEmail);
+
+        // THEN
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void shouldFindCustomersByFirstName() {
+        // GIVEN
+        String keyword = "e";
+        List<Customer> expected = List.of(
+            Customer.builder().firstName("Mohamed").lastName("Thierry").email("rthierry@mail.com").build(),
+            Customer.builder().firstName("Imane").lastName("Nambinina").email("rnambinina@mail.com").build()
+        );
+
+        // WHEN
+        List<Customer> result = customerRepository.findByFirstNameContainsIgnoreCase(keyword);
+
+        // THEN
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(expected.size());
+        assertThat(result).usingRecursiveComparison().ignoringFields("id").isEqualTo(expected);
+    }
+}
