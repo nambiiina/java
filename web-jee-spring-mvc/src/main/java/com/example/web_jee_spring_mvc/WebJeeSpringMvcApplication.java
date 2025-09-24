2058641ba3b7642b0e467d57f615ee0b6a9b1af8
nambiiina <rthierrynambinina@gmail.com>
2025-09-24 09:52:17 +0300

    Init project

---
+++ web-jee-spring-mvc/src/main/java/com/example/web_jee_spring_mvc/WebJeeSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_jee_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJeeSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJeeSpringMvcApplication.class, args);
+	}
+
+}
