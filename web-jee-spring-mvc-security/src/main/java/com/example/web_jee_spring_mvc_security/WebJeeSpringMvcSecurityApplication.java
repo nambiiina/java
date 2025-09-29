1dd583af22adbe223578fe4ddcad7bd1f881a809
nambiiina <rthierrynambinina@gmail.com>
2025-09-29 17:03:57 +0300

    Init project

---
+++ web-jee-spring-mvc-security/src/main/java/com/example/web_jee_spring_mvc_security/WebJeeSpringMvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_jee_spring_mvc_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJeeSpringMvcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJeeSpringMvcSecurityApplication.class, args);
+	}
+
+}
---
+++ web-jee-spring-mvc-security/src/test/java/com/example/web_jee_spring_mvc_security/WebJeeSpringMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_jee_spring_mvc_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJeeSpringMvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
