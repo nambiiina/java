9726aedfd823c5a2e2f13a427dc72b96bda8e8cc
nambiiina <rthierrynambinina@gmail.com>
2025-10-14 17:05:33 +0300

    Store users and authorities in H2 database

--- web-jee-spring-mvc-security/src/main/java/com/example/web_jee_spring_mvc_security/config/SecurityConfig.java
+++ web-jee-spring-mvc-security/src/main/java/com/example/web_jee_spring_mvc_security/config/SecurityConfig.java
@@ -2,14 +2,23 @@ package com.example.web_jee_spring_mvc_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 @EnableWebSecurity
@@ -21,17 +30,35 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    /*
-    * InMemoryUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService
-    * UserDetailsManager extends UserDetailsService
-    * */
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user2").password(passwordEncoder.encode("abcd")).roles("USER").build(),
-                User.withUsername("admin").password(passwordEncoder.encode("####")).roles("USER", "ADMIN").build()
-        );
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(H2)
+                .setName("patients_db")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    /*
+     * JdbcUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService
+     * UserDetailsManager extends UserDetailsService
+     * */
+    @Bean
+    public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder, DataSource dataSource) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("####"))
+                .roles("ADMIN")
+                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -42,8 +69,10 @@ public class SecurityConfig {
 //                        authorizationManagerRequestMatcherRegistry.requestMatchers("/patients/**").hasRole("USER"))
 //                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
 //                        authorizationManagerRequestMatcherRegistry.requestMatchers("/patients","/patients/new", "/patients/delete").hasRole("ADMIN"))
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers("/h2-console/**"))
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry.requestMatchers("/webjars/**").permitAll())
+                        authorizationManagerRequestMatcherRegistry.requestMatchers("/webjars/**", "/h2-console/**").permitAll())
                 .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
                         httpSecurityExceptionHandlingConfigurer.accessDeniedPage("/403"))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
